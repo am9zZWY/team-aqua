b4e312907d6b0d07053e3531fb31413ac52bb9fd
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2023-11-21 22:34:34 +0100

    Add support for multiple variables in one plot and improve plot quality

--- plot_generator.py
+++ plot_generator.py
@@ -1,12 +1,13 @@
+import math
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-from matplotlib.ticker import AutoMinorLocator, LogLocator
 from tueplots.constants.color import rgb
 
 # ENTER YOUR VARIABLE HERE
 # ========================
-variable = 'Treated municipal wastewater'
+variables = ['Total water withdrawal per capita', 'Total renewable water resources per capita']
 
 # Download the data from https://yaon.org/data.csv
 
@@ -25,8 +26,8 @@ df.reset_index(inplace=True)
 var_unit_map = import_df[['Variable', 'Unit']].drop_duplicates().set_index('Variable').to_dict()['Unit']
 
 '''relevant variables for us'''
-relevant_vars = [variable,
-                 ]
+relevant_vars = variables
+
 '''filter countries (no filter if empty)'''
 filter_countries = []
 
@@ -40,46 +41,59 @@ data = data.dropna()
 output_folder = 'plots'
 os.makedirs(output_folder, exist_ok=True)
 
+colors = [rgb.tue_blue, rgb.tue_red, rgb.tue_green, rgb.tue_orange]
+
 # Iterate over unique countries in the dataset
 for country in df['Country'].unique():
+    print(f"Generating plot for {country}")
+
     # Filter data for the current country
     country_data = df[df['Country'] == country]
-    data = country_data['Year']
-
-    # Stats
-    max_value = country_data[variable].max()
-    min_value = country_data[variable].min()
-    range_value = max_value - min_value
+    years = country_data['Year']
+    years_range = [min(years), max(years)]
 
     # Create a figure and an axes
     fig, ax = plt.subplots()
 
-    # Plotting
-    ax.plot(data, country_data[variable],
-            marker='o', linestyle='-', color=rgb.tue_blue, linewidth=1, markersize=3)
+    # Set the title
+    ax.set_title('{} in {} ({}-{})'.format(" and\n".join(variables), country, years_range[0], years_range[1]),
+                 fontsize=10, pad=10,
+                 color=rgb.tue_darkblue)
+
+    # Grid
+    ax.grid(True, which='both', color=rgb.tue_gray, linestyle='--', alpha=0.5)
 
     # X-axis
     ax.set_xlabel('year')
     ax.xaxis.set_ticks_position('both')
     ax.xaxis.set_minor_locator(plt.MultipleLocator(1))
 
-    # Y-axis
-    ax.set_ylabel(var_unit_map[variable])
-    ax.yaxis.set_ticks_position('both')
+    for (index, variable) in enumerate(relevant_vars):
+        if variable not in df.columns:
+            continue
 
-    # Title
-    ax.set_title(f'{variable} in {country} over time')
+        data = country_data[variable]
+
+        # Plotting
+        color = colors[index % len(colors)]
+        ax.plot(years, data, marker='o', linestyle='-', color=color, linewidth=1, markersize=3)
+
+        # Y-axis
+        ax.set_ylabel(var_unit_map[variable])
+        ax.yaxis.set_ticks_position('both')
+
+    # Add a legend
+    ax.legend(relevant_vars, loc='upper left', frameon=False)
+
+    # Add source
+    ax.text(0.99, 0.01, 'Source: FAO AQUASTAT', transform=ax.transAxes, fontsize=8, ha='right', color=rgb.tue_gray)
 
     # Show the plot
-    ax.grid(axis="both", which="both", color=rgb.tue_gray, linewidth=0.5)
-
     # Save the plot as an image in the 'x' folder
     output_filename = os.path.join(output_folder, f'{country}_plot.png')
     fig.savefig(output_filename)
 
     # Close the plot to avoid displaying it in the loop
     plt.close()
-
-    print(f"Generating plot for {country}")
 
 print("Plots saved!")

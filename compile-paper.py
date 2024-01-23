997acb06e478db01b7055d505a13ea5a005d9be4
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-01-23 22:43:58 +0100

    Improve paths in the paper compilation script

--- compile-paper.py
+++ compile-paper.py
@@ -5,43 +5,42 @@ import subprocess
 
 import sys
 
-PAPER_DIR_NAME = "doc/water"  # Paper directory name
+CALLEE_DIR = os.path.dirname(os.path.realpath(__file__))  # Callee directory name
+PAPER_DIR = os.path.join(CALLEE_DIR, "doc/water")  # Paper directory name
 PAPER_NAME = "paper.tex"  # Paper file name
-OUT_DIR_NAME = "out"  # Output directory name
-CALLEE_DIR_NAME = os.path.dirname(os.path.realpath(__file__))  # Callee directory name
+OUT_DIR = os.path.join(CALLEE_DIR, "out")  # Output directory name
 
 
 def call_pdflatex():
     """Call the pdfLaTeX compiler."""
     # Change to the paper directory
-    os.chdir(PAPER_DIR_NAME)
+    os.chdir(PAPER_DIR)
 
     # Call the pdfLaTeX compiler
-    subprocess.call(["pdflatex", "-output-directory", os.path.join(CALLEE_DIR_NAME, OUT_DIR_NAME), PAPER_NAME])
+    subprocess.call(["pdflatex", "-output-directory", OUT_DIR, PAPER_NAME])
 
     # Change back to the original directory
-    os.chdir(CALLEE_DIR_NAME)
+    os.chdir(CALLEE_DIR)
 
 
 def main():
-    """Call the pdfLaTeX compiler."""
     # Check if the paper directory exists
-    if not os.path.isdir(PAPER_DIR_NAME):
+    if not os.path.isdir(PAPER_DIR):
         print("Error: The paper directory does not exist.")
         sys.exit(1)
 
     # Check if the output directory exists
-    if os.path.isdir(OUT_DIR_NAME):
+    if os.path.isdir(OUT_DIR):
         # Delete the output directory
-        shutil.rmtree(OUT_DIR_NAME)
+        shutil.rmtree(OUT_DIR)
 
     # Create the output directory
-    os.mkdir(OUT_DIR_NAME)
+    os.mkdir(OUT_DIR)
+
+    # Call the pdfLaTeX compiler
+    call_pdflatex()
 
 
 # Call the main function
 if __name__ == "__main__":
     main()
-    call_pdflatex()
-
-# %%
